2ea73927bf7a0ad568f592483f0b8a69fc2f9dae
LeonardaSaad <leonardassaad777@gmail.com>
2024-09-22 17:56:43 -0300

    feat: new tabs of employees and register in/out

--- src/App.jsx
+++ src/App.jsx
@@ -1,143 +1,257 @@
 import { useState } from "react";
 import {
-	Card,
-	CardContent,
-	CardFooter,
-	CardHeader,
-	CardTitle,
-	CardDescription,
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+  CardDescription,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { Button } from "@/components/ui/button";
-import AlertDialogComponent from "./components/AlertDialogComponent";
+import AlertDialogComponent from "@/components/AlertDialogComponent";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 import axios from "axios";
+import { Button } from "./components/ui/button";
 
 export default function App() {
-	const [funcionarioId, setFuncionarioId] = useState("");
-	const [funcionarioName, setFuncionarioName] = useState("");
+  const [funcionarioId, setFuncionarioId] = useState("");
+  const [funcionarioName, setFuncionarioName] = useState("");
   const [dados, setDados] = useState("");
 
-	const registerEmployee = async (e) => {
-		e.preventDefault();
+  const registerEmployee = async (e) => {
+    e.preventDefault();
 
     const data = {
-      "name": funcionarioName,
-    }
+      name: funcionarioName,
+    };
 
-    // event.target.value = ''
-
-    setFuncionarioName('')
+    setFuncionarioName("");
 
     try {
       // Enviando a requisição POST
-      const response = await axios.post('http://localhost:8080/funcionarios', data);
+      const response = await axios.post(
+        "http://localhost:8080/funcionarios",
+        data
+      );
 
       setDados(response.data);
-
-
     } catch (error) {
-      console.error('Erro na requisição:', error);
+      console.error("Erro na requisição:", error);
     }
-    
-	
-	};
-
+  };
 
   const registerPoint = async (e) => {
-		e.preventDefault();
+    e.preventDefault();
 
     const data = {
-      "funcionario_id": funcionarioId,
-    }
+      funcionario_id: funcionarioId,
+    };
 
-    // event.target.value = ''
-
-    setFuncionarioId('')
+    setFuncionarioId("");
 
     try {
       // Enviando a requisição POST
-      const response = await axios.post('http://localhost:8080/registro_ponto', data);
+      const response = await axios.post(
+        "http://localhost:8080/registro_ponto",
+        data
+      );
 
       setDados(response.data);
-
-
     } catch (error) {
-      console.error('Erro na requisição:', error);
+      console.error("Erro na requisição:", error);
     }
-    
-	
-	};
-	return (
-		<>
-			<h1 className="font-bold text-center m-10 text-2xl">CHD Ponto Digital</h1>
-			<Tabs defaultValue="point-record" className="w-[400px]">
-				<TabsList className="flex justify-center space-x-2">
-					<TabsTrigger value="point-record">Registro de Ponto</TabsTrigger>
-					<TabsTrigger value="register-user">
-						Cadastro de Funcionários
-					</TabsTrigger>
-				</TabsList>
+  };
+
+  const getAllFuncionarios = async () => {
+    try {
+      const response = await axios.get("http://localhost:8080/funcionarios");
+
+      if (response.data) {
+        setDados(response.data);
+      } else {
+        console.error("Nenhum dado retornada da API.");
+      }
+    } catch (error) {
+      console.error("Erro na requisição:", error);
+    }
+  };
+  const getAllPontoRegistros = async () => {
+    try {
+      const response = await axios.get("http://localhost:8080/registro_ponto");
+
+      if (response.data) {
+        setDados(response.data);
+      } else {
+        console.error("Nenhum dado retornada da API.");
+      }
+    } catch (error) {
+      console.error("Erro na requisição:", error);
+    }
+  };
+
+  return (
+    <>
+      <h1 className="font-bold text-center m-10 text-2xl">CHD Ponto Digital</h1>
+      <Tabs
+        defaultValue="point-record"
+        className="w-max"
+        onValueChange={(value) => {
+          if (value === "funcionarios") {
+            getAllFuncionarios();
+          } else if (value === "registro_ponto") {
+            getAllPontoRegistros();
+          }
+        }}
+      >
+        <TabsList className="flex justify-center space-x-2">
+          <TabsTrigger value="point-record">Bater Ponto</TabsTrigger>
+          <TabsTrigger value="register-user">
+            Cadastro de Funcionários
+          </TabsTrigger>
+          <TabsTrigger value="funcionarios">Funcionários</TabsTrigger>
+          <TabsTrigger value="registro_ponto">Pontos Cadastrados</TabsTrigger>
+        </TabsList>
         {/* REGISTRAR PONTO */}
-				<TabsContent value="point-record">
-					<Card className="w-full max-w-md mx-auto">
-						<CardHeader>
-							<CardTitle>Registro de Ponto</CardTitle>
-							<CardDescription>
-								Insira seu ID para registrar o ponto
-							</CardDescription>
-						</CardHeader>
-						<form onSubmit={registerPoint}>
-							<CardContent className="space-y-4">
-								<div className="space-y-2">
-									<Label htmlFor="funcionarioId">ID do Funcionário</Label>
-									<Input
-										id="funcionarioId"
-										placeholder="Digite seu ID"
-										value={funcionarioId}
-										onChange={(e) => setFuncionarioId(e.target.value)}
-										required
-									/>
-								</div>
-							</CardContent>
-							<CardFooter>
+        <TabsContent value="point-record">
+          <Card className="w-full mx-auto">
+            <CardHeader>
+              <CardTitle>Registro de Ponto</CardTitle>
+              <CardDescription>
+                Insira seu ID para registrar o ponto
+              </CardDescription>
+            </CardHeader>
+            <form onSubmit={registerPoint}>
+              <CardContent className="space-y-4">
+                <div className="space-y-2">
+                  <Label htmlFor="funcionarioId">ID do Funcionário</Label>
+                  <Input
+                    id="funcionarioId"
+                    placeholder="Digite seu ID"
+                    value={funcionarioId}
+                    onChange={(e) => setFuncionarioId(e.target.value)}
+                    required
+                  />
+                </div>
+              </CardContent>
+              <CardFooter>
                 <AlertDialogComponent dados={dados} isRegister={false} />
-							</CardFooter>
-						</form>
-					</Card>
-				</TabsContent>
+              </CardFooter>
+            </form>
+          </Card>
+        </TabsContent>
 
         {/* CADASTRO DE FUNCIONÁRIO */}
-				<TabsContent value="register-user">
-					<Card className="w-full max-w-md mx-auto">
-						<CardHeader>
-							<CardTitle>Cadastro de Funcionários</CardTitle>
-							<CardDescription>
-								Insira nome completo do funcionário que deseja cadastrar.
-							</CardDescription>
-						</CardHeader>
-						<form onSubmit={registerEmployee}>
-							<CardContent className="space-y-4">
-								<div className="space-y-2">
-									<Label htmlFor="nome">Nome do Funcionário</Label>
-									<Input
-										id="nome"
-										placeholder="Digite seu nome completo"
-										value={funcionarioName}
-										onChange={(e) => setFuncionarioName(e.target.value)}
-										required
-									/>
-								</div>
-							</CardContent>
-							<CardFooter>
+        <TabsContent value="register-user">
+          <Card className="w-full mx-auto">
+            <CardHeader>
+              <CardTitle>Cadastro de Funcionários</CardTitle>
+              <CardDescription>
+                Insira nome completo do funcionário que deseja cadastrar.
+              </CardDescription>
+            </CardHeader>
+            <form onSubmit={registerEmployee}>
+              <CardContent className="space-y-4">
+                <div className="space-y-2">
+                  <Label htmlFor="nome">Nome do Funcionário</Label>
+                  <Input
+                    id="nome"
+                    placeholder="Digite seu nome completo"
+                    value={funcionarioName}
+                    onChange={(e) => setFuncionarioName(e.target.value)}
+                    required
+                  />
+                </div>
+              </CardContent>
+              <CardFooter>
                 <AlertDialogComponent dados={dados} />
-							</CardFooter>
-						</form>
-					</Card>
-				</TabsContent>
-			</Tabs>
-		</>
-	);
+              </CardFooter>
+            </form>
+          </Card>
+        </TabsContent>
+
+        {/* TABELA FUNCIONÁRIOS */}
+        <TabsContent value="funcionarios">
+          <Card className="w-full mx-auto">
+            <CardHeader>
+              <CardTitle>Funcionários</CardTitle>
+              <CardDescription>
+                Aqui ficam todos os funcionários cadastrados.
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="space-y-4">
+              <Table>
+                <TableHeader>
+                  <TableRow>
+                    <TableHead className="w-[100px]">ID</TableHead>
+                    <TableHead>Funcionário</TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  {Array.isArray(dados) &&
+                    dados.map((funcionario, index) => (
+                      <TableRow key={index}>
+                        <TableCell className="font-medium">
+                          {funcionario.funcionario_id}
+                        </TableCell>
+                        <TableCell>{funcionario.name}</TableCell>
+                      </TableRow>
+                    ))}
+                </TableBody>
+              </Table>
+            </CardContent>
+          </Card>
+        </TabsContent>
+
+        {/* TABELA REGISTROS */}
+        <TabsContent value="registro_ponto">
+          <Card className="w-full mx-auto">
+            <CardHeader>
+              <CardTitle>Registro de Pontos</CardTitle>
+              <CardDescription>
+                Aqui ficam todos os registros de pontos.
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="space-y-4">
+              <Table>
+                <TableHeader>
+                  <TableRow>
+                    <TableHead className="w-[100px]">ID do Ponto</TableHead>
+                    <TableHead>Funcionário</TableHead>
+                    <TableHead>Entrada</TableHead>
+                    <TableHead>Saída</TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  {Array.isArray(dados) &&
+                    dados.map((ponto, index) => (
+                      <TableRow key={index}>
+                        <TableCell>{ponto.pontoId}</TableCell>
+                        <TableCell>
+                          {ponto.funcionario
+                            ? ponto.funcionario.name
+                            : "Desconhecido"}
+                        </TableCell>
+                        <TableCell>{ponto.dataEntradaConverted}</TableCell>
+                        <TableCell>{ponto.dataSaidaConverted}</TableCell>
+                      </TableRow>
+                    ))}
+                </TableBody>
+              </Table>
+            </CardContent>
+          </Card>
+        </TabsContent>
+      </Tabs>
+    </>
+  );
 }
